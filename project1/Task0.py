c3df56edda37e227d68d071bef2ec1232d2cb9d7
Jessica Hsu <hsujessica21@gmail.com>
2021-02-13 13:01:28 -0600

    finish task0

--- project1/Task0.py
+++ project1/Task0.py
@@ -20,3 +20,20 @@ Print messages:
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
 
+# find first record of text
+first_text = texts[0]
+print(f"First record of texts, {first_text[0]} texts {first_text[1]} at time {first_text[2]}")
+
+# find last record of calls
+last_call = calls[-1]
+print(f"Last record of calls, {last_call[0]} calls {last_call[1]} at time {last_call[2]}, lasting {last_call[3]} seconds")
+
+
+"""
+Run time Analysis:
+Worst case scenario for run time is O(1). The calls and texts were read from csv and then put into an array.
+Worst case lookup for arrays are O(1).  Array lookups were used to determine first text and last call (two lookups).
+Three more lookups were used to text sender, text receiver, and text time. Four lookups were used to get
+caller, receiver, call time, and call length. Total of 2 + 3 + 4 = 9 lookups were made. 9 x O(1) is stil O(1).
+
+"""
--- project1/Task1.py
+++ project1/Task1.py
@@ -18,3 +18,13 @@ How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+
+
+"""
+Run time Analysis:
+Worst case scenario for run time is O(1). The calls and texts were read from csv and then put into an array.
+Worst case lookup for arrays are O(1).  Array lookups were used to determine first text and last call (two lookups).
+Three more lookups were used to text sender, text receiver, and text time. Four lookups were used to get
+caller, receiver, call time, and call length. Total of 2 + 3 + 4 = 9 lookups were made. 9 x O(1) is stil O(1).
+
+"""
