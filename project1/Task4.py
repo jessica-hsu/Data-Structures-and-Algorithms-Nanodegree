4b562c21b46e31bcb68a39ecfb3a1667fcdb8eba
Jessica Hsu <hsujessica21@gmail.com>
2021-03-19 01:58:10 -0500

    finish task 4 and analysis

--- project1/Task4.py
+++ project1/Task4.py
@@ -25,3 +25,35 @@ Print a message:
 The list of numbers should be print out one per line in lexicographic order with no duplicates.
 """
 
+# find unique list of outgoing callers and call receivers
+outgoing_callers = []
+received_calls = []
+for call in calls:
+    caller = call[0]
+    callee = call[1]
+    if (caller not in outgoing_callers):
+        outgoing_callers.append(caller)
+    if (callee not in received_calls):
+        received_calls.append(callee)
+
+# find unique list of text senders and text receivers
+text_senders = []
+text_recievers = []
+for text in texts:
+    texter = text[0]
+    textee = text[1]
+    if (texter not in text_senders):
+        text_senders.append(texter)
+    if (textee not in text_recievers):
+        text_recievers.append(textee)
+
+# iterate through list of outgoing callers and see if they exist in the remaining three lists. If they do, remove
+for caller in outgoing_callers:
+    if (caller in received_calls or caller in text_senders or caller in text_recievers):
+        outgoing_callers.remove(caller)
+
+# print statement
+outgoing_callers = sorted(outgoing_callers)
+print("These numbers could be telemarketers: ")
+for caller in outgoing_callers:
+    print(caller)
